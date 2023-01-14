32c465309a9031f410ee9807ad557540d0a57c72
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2023-01-14 11:22:15 +0100

    add sign up functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import { ForgotPassword, Home, Offers, Profile, SignIn, SignUp } from './pages'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { ForgotPassword, Home, Offers, Profile, SignIn, SignUp } from './pages';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import { Header } from './components'
+import { Header } from './components';
+import { ToastContainer } from 'react-toastify';
 
 // import ForgotPassword from './pages'
-
 
 const App = () => {
   return (
     <>
-    <Router>
-      <Header />
-      {/* <ForgotPassword /> */}
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/offers' element={<Offers />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Header />
+        {/* <ForgotPassword /> */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/offers' element={<Offers />} />
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+ />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
